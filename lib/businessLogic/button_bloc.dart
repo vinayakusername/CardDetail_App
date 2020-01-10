26b15bc80a8dfa5ff2376a0c3215ac2af5a489a9
vinayakusername <gpraut88@gmail.com>
2020-01-10 17:57:45 +0530

    updated

--- lib/businessLogic/button_bloc.dart
+++ lib/businessLogic/button_bloc.dart
@@ -1,9 +1,9 @@
 // This file contain Methods which return Card Value on press.
 
 //this method will return the first and last values in card based on pressed button number.
-void returnCardDetail()
+void returnCardDetail(int buttonID)
 {
-  // if(buttonID == btn1){
+  // if(buttonID == btn1_ID){
   //      return First value in card from list.
   // }
 
