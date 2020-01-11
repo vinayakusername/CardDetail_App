e911f4730f463ef00b97dce801b6fd5257ce02ab
vinayakusername <gpraut88@gmail.com>
2020-01-11 12:39:58 +0530

    updated

--- lib/Provider/Provider1.dart
+++ lib/Provider/Provider1.dart
@@ -10,13 +10,19 @@ import 'package:flutter/cupertino.dart';
 //to access the members of Provider1 class.
 class Provider1 extends ChangeNotifier
 {
+    // Provider1.returnFirstValue() is named constructor , here Provider1 is class 
+    //and returnFirstValue is constructor name.
 
-    void returnFirstValue(){
-     // notifyListeners();
-    }  // this function will return First Data of list in cards.
+    // Provider1.returnFirstValue(this.object_of_bloc_class){
+    //  // notifyListeners();
+    // }  // this function will return First Data of list in cards.
 
 
-    void returnLastValue(){
-    //  notifyListeners(); // This function will notify to the listener about method.
-    }  //This function will return Last Data of list in cards.
+
+      // Provider1.returnLastValue() is named constructor , here Provider1 is class 
+    //and returnLastValue is constructor name.
+
+    // Provider1.returnLastValue(this.object_of_bloc_class){
+    // //  notifyListeners(); // This function will notify to the listener about method.
+    // }  //This function will return Last Data of list in cards.
 }
